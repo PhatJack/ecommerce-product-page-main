b9ac6fa784c9093f65c5207661c6c86d66d345b3
PhatJack <tienphatng.693@gmail.com>
2024-01-15 15:48:47 +0700

    final solution

--- src/components/Header/MobileMenu.jsx
+++ src/components/Header/MobileMenu.jsx
@@ -1,22 +1,32 @@
 import React from 'react'
 
-const MobileMenu = ({ menu }) => {
+const MobileMenu = ({ menu, mobile, setMobile }) => {
 	return (
 		<>
-			<div className='block md:hidden absolute w-3/4 h-screen bg-white inset-0'>
-				<ul className='flex flex-col gap-8 text-dark-grayish-blue'>
-					{
-						menu.map((item, index) => (
-							<li key={index} className='relative w-full group'>
-								<span key={index} className='cursor-pointer' >
-									{item}
-								</span>
-								<span className='absolute w-full h-1 bg-orange left-0 -bottom-[2.75em] transform scale-0 group-hover:scale-100 transition-all duration-[400ms]'></span>
-							</li>
-						))
-					}
-				</ul>
+			<div className={`block md:hidden fixed w-3/4 h-screen bg-white top-0 left-0 ${mobile ? "translate-x-0" : "translate-x-full"} transition-all duration-500 z-[2]`}>
+				<div className="w-full h-full flex flex-col gap-20 py-10 px-7">
+					<button type='button' onClick={() => setMobile(false)} className="w-full">
+						<svg width="14" height="15" xmlns="http://www.w3.org/2000/svg"><path d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z" fill="#69707D" fill-rule="evenodd" /></svg>
+					</button>
+					<ul className='flex flex-col gap-8 text-dark-grayish-blue'>
+						{
+							menu.map((item, index) => (
+								<li key={index} className='relative w-full group'>
+									<span key={index} className='cursor-pointer text-lg text-black font-bold' >
+										{item}
+									</span>
+								</li>
+							))
+						}
+					</ul>
+				</div>
 			</div>
+			{
+				mobile && (
+					<div className="fixed w-full inset-0 h-screen z-[1] bg-black/10">
+					</div>
+				)
+			}
 		</>
 
 
